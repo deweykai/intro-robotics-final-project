547cbbf1e020e1991a313fb9f00d3e246428163d
Kai <deweykai5@gmail.com>
2022-11-02 16:04:28 -0600

    gps positioning

--- controllers/grocery_shopper/localization.py
+++ controllers/grocery_shopper/localization.py
@@ -1,7 +1,24 @@
+import math
+from robot import gps, compass
+
 # Odometry
 pose_x = 0
 pose_y = 0
 pose_theta = 0
+
+
+class GPS:
+    def update(self):
+        global pose_x, pose_y, pose_theta
+        pose_x = gps.getValues()[0]
+        pose_y = gps.getValues()[1]
+
+        n = compass.getValues()
+        rad = ((math.atan2(n[0], -n[2])))
+        pose_theta = rad
+
+
+positioning = GPS()
 
 
 def init():
@@ -9,4 +26,6 @@ def init():
 
 
 def update():
-    pass
+    positioning.update()
+
+    print(pose_x, pose_y)
