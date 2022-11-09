741dd72298ede4ff86bcfe95d950e30bdaaa0200
Kai <deweykai5@gmail.com>
2022-11-09 14:59:07 -0700

    lidar stuff

--- controllers/grocery_shopper/vision.py
+++ controllers/grocery_shopper/vision.py
@@ -11,7 +11,9 @@ This module depends on:
 
 
 import numpy as np
+import localization as loc
 import math
+from robot import lidar
 
 LIDAR_ANGLE_BINS = 667
 LIDAR_SENSOR_MAX_RANGE = 5.5  # Meters
@@ -23,6 +25,32 @@ lidar_offsets = np.linspace(-LIDAR_ANGLE_RANGE /
                             2., +LIDAR_ANGLE_RANGE/2., LIDAR_ANGLE_BINS)
 # Only keep lidar readings not blocked by robot chassis
 lidar_offsets = lidar_offsets[83:len(lidar_offsets)-83]
+
+
+def get_lidar_readings():
+    readings = []
+    lidar_sensor_readings = lidar.getRangeImage()
+    lidar_sensor_readings = lidar_sensor_readings[83:len(
+        lidar_sensor_readings)-83]
+    for i, rho in enumerate(lidar_sensor_readings):
+        alpha = lidar_offsets[i]
+
+        if rho > LIDAR_SENSOR_MAX_RANGE:
+            continue
+
+        # The Webots coordinate system doesn't match the robot-centric axes we're used to
+        rx = -math.cos(alpha)*rho + 0.202
+        ry = math.sin(alpha)*rho - 0.004
+
+        # Convert detection from robot coordinates into world coordinates
+        wx = math.cos(loc.pose_theta)*rx - \
+            math.sin(loc.pose_theta)*ry + loc.pose_x
+        wy = +(math.sin(loc.pose_theta)*rx +
+               math.cos(loc.pose_theta)*ry) + loc.pose_y
+
+        readings.append([wx, wy])
+
+    return readings
 
 
 def init():
